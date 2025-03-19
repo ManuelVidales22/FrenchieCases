6d4acfefeea2541cd236e9017fbe32f44c0007eb
ManuelVidales22 <manuelvidales33@gmail.com>
2025-03-19 09:58:02 -0500

    Proyecto Franchie Cases

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,19 @@
+import { ReactiveFormsModule } from '@angular/forms';
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [
+    ReactiveFormsModule,
+    RouterOutlet,
+    CommonModule], // Importa el componente TarjetaComponent aquí
+
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css'] // Corregido de styleUrl a styleUrls
 })
 export class AppComponent {
   title = 'FrenchieCases';
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,23 @@
-import { Routes } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import {LoginComponent} from './components/login/login.component';
+import { RegistroComponent } from './components/registro/registro.component';
 
-export const routes: Routes = [];
+
+
+export const routes: Routes = [
+    {path: '', pathMatch: 'full', redirectTo: 'login'},
+    {path: 'login', component: LoginComponent},
+    {path: 'registro', component: RegistroComponent },
+
+    {path: '**', redirectTo: 'login'}
+];
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes, {
+      useHash: true  // Esto puede ayudar con el enrutamiento en GitHub Pages
+    })],
+    exports: [RouterModule]
+  })
+  export class AppRoutes {}
+
